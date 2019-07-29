b6c1af1f4b9cb1cb1efb069b269b82e70987efb4
Jensine Burk <developer55@trialcard.com>
2019-07-29 19:29:29 -0400

    updates

---
+++ src/components/MovieCard.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-card>
+    <v-img :src="movie.img"/>
+    <v-card-title>
+      <h2>{{movie.title}}</h2>
+      <v-rating medium v-model="editRating"></v-rating>
+      <p>{{`${movie.rating}/5`}}</p>
+    </v-card-title>
+  </v-card>
+</template>
+
+<style>
+</style>
+
+<script>
+export default {
+  props: {
+    movie: Object
+  },
+  data: () => ({
+    editRating: 0
+  }),
+  created() {
+    this.editRating = this.$props.movie.rating;
+  },
+ watch: {
+    editRating: function() {
+      this.$emit("change-rating", this.editRating);
+    }
+  }
+};
+</script>
